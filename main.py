87ab1b9e8fef36c2c0cb6d2dad7e84d7e62824b4
Yackob234 <bloo789@yahoo.ca>
2024-07-23 13:46:05 -0400

    TB change on CORS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI;
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from Endpoints.Endpoints import router
 InfoGrepChatroomService = FastAPI();
 
 InfoGrepChatroomService.include_router(router)
+InfoGrepChatroomService.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"], allow_credentials=True)
 if __name__ == "__main__":
     uvicorn.run(InfoGrepChatroomService, host="0.0.0.0", port=8003)
